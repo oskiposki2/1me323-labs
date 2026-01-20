bbc732d6974b7b4e1b5c1e41c41ab919b7a0ef0d
Oskar Berntson <oskar.berntson@gmail.com>
2026-01-20 14:59:39 +0100

    Initial commit with math functions

---
+++ 1a/main.js
@@ -0,0 +1,20 @@
+import * as MathUtils from './math.js'
+
+import { add, subtract, multiply, PI} from './math.js'
+
+console.log(MathUtils.add(2, 3))
+
+console.log(MathUtils.subtract(22, 10))
+
+console.log(MathUtils.multiply(2, 30))
+
+import divide from './math.js'
+
+console.log(divide(10, 2))
+
+console.log(divide(PI, chicken))
+
+import { SECRET_SAUCE as chicken } from './math.js'
+
+console.log(add(2, chicken))
+
---
+++ 1a/math.js
@@ -0,0 +1,18 @@
+export function add (a, b) {
+    return a + b;
+}
+
+export function subtract (a, b) {
+    return a - b;
+}
+
+export function multiply (a, b) {
+    return a * b;
+}
+
+export default function divide (a, b) {
+    return a / b;
+}
+
+export const PI = 3.14159
+export const SECRET_SAUCE = 42
